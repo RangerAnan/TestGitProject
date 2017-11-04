4dfb51c558b4b27af603e8c16b555f4e349888e1
fangchaolai <`1234fcl>
2017-11-04 15:40:01 +0800

    创建git测试页面

--- app/src/main/java/com/test/mi/testproject/MainActivity.java
+++ app/src/main/java/com/test/mi/testproject/MainActivity.java
@@ -1,13 +1,48 @@
 package com.test.mi.testproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.test.mi.testproject.git.TestGitActivity;
+
+public class MainActivity extends AppCompatActivity implements OnItemClickListener {
+
+    private ListView listView;
+    private String[] item = {"TestGit"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+        initData();
+    }
+
+    private void initView() {
+        listView = findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, R.layout.lv_main_item, R.id.tv_main, item);
+        listView.setAdapter(adapter);
+    }
+
+
+    private void initData() {
+        listView.setOnItemClickListener(this);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        Intent intent = new Intent();
+        if (position == 0) {
+            intent.setClass(MainActivity.this, TestGitActivity.class);
+        }
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/test/mi/testproject/git/TestGitActivity.java
@@ -0,0 +1,15 @@
+package com.test.mi.testproject.git;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.test.mi.testproject.R;
+
+public class TestGitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_git);
+    }
+}
